b994598f1c234c0af06ef669d38223ac5e828c0f
Ken Davidson <ken.j.davidson@live.ca>
2022-12-02 14:59:25 +0000

    fix(build): fix js and style building

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
 const path = require('path');
 
 const isProduction = process.env.NODE_ENV == "production";
+const buildTime = Math.floor(Date.now() / 1000);
+const prodPlugins = isProduction ? [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "./_site/index.html"),
+    inject: false
+  }),
+  new HtmlReplaceWebpackPlugin([{
+    pattern: 'bundle.js',
+    replacement: `bundle.${buildTime}.js`
+  },{
+    pattern: 'styles.css',
+    replacement: `styles.${buildTime}.js`
+  }])
+] : [];
 
 const config = {
-  entry: "./src/stimulus.ts",
+  entry: {
+    bundle: "./src/stimulus.ts",
+    styles: "./css/styles.css"
+  },
   output: {
-    filename: 'bundle.js',
+    filename: isProduction ? `[name].${buildTime}.js` : `[name].js`,
     path: path.resolve(__dirname, "./_site"),
   },
-  plugins: [],
+  plugins: [...prodPlugins],
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
         exclude: ["/node_modules/"],
-      },
+      },{
+        test: /\.css$/i,
+        use: ['css-loader', 'postcss-loader']
+      }
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-  },
+  }
 };
 
 module.exports = () => {
